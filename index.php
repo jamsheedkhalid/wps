229f026e84ca68b5b4d8a121e40fda57c71b2379
jamsheedkhalid <jamsheedkhalid35@gmail.com>
2019-07-08 16:32:37 +0400

    SIF Generation

--- index.php
+++ index.php
@@ -17,7 +17,7 @@ session_start();
         <script src="js/bootstrap-datetimepicker.min.js"></script>
         <script src="plugins/bootstrap/js/bootstrap.js"></script>
         <script src="plugins/bootstrap/js/bootstrap.min.js"></script>
-        <script type="text/javascript" src="js/html2CSV.js" ></script>
+        <script src="js/html2CSV.js" ></script>
 <!--        <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo"  rossorigin="anonymous"></script>-->
         <script src="js/jquery.tabletocsv.js"></script>
 
